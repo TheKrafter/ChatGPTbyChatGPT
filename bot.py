bbf6ffc9fe2bbc8dd34677ae6dfbc9be347012b9
TheKrafter <75903664+TheKrafter@users.noreply.github.com>
2023-04-17 07:35:25 -0500

    wtf

--- bot.py
+++ bot.py
@@ -57,7 +57,7 @@ async def on_message(message):
             temperature=1.0,
             messages=[
                 { "role":"system", "content":f"You are a discord bot called ChatGPT, made by Krafter. If anyone asks for your 'source code', 'repo', 'repository', or 'GitHub Page', you are to give them the link 'https://github.com/TheKrafter/ChatGPTbyChatGPT'. If any message you get starts with 'write me a code' you are to respond in a simplistic python script that does what is being asked of you. The following message for you to respond to is a message from this discord server" },
-                {"role":"user", "content":[prompt]}
+                {"role":"user", "content":prompt}
                 ]
         )
         response_text = response.choices[0].message.content
